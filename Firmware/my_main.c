a1ca75e7d48c077a2b513f4ce8bf068ad76f06c9
Bagó Bálint <bago.balint10@gmail.com>
2025-11-03 23:29:24 +0100

    final v2 interpolálással

--- Firmware/my_main.c
+++ Firmware/my_main.c
@@ -19,10 +19,17 @@
 #define	TILT_F_CH 14
 #define	PT_SPEED_CH 15
 
+#define SM_TIME 5							// 70ms egy frame
+#define SM_COUNT 17							// 85 ms mintavételezés
+#define SM_COUNT_REV 0.058824f				// 17 db interpolálás 5ms enként
+
 static uint16_t dmx_pan_value = 0;
 static uint16_t dmx_tilt_value = 0;
 static uint8_t  dmx_speed_value = 0;
-static uint8_t  reset_f = 0; 			// 0--reset,  > 0 fut a program
+static uint8_t  reset_f = 0; 				// 0--reset,  > 0 fut a program
+
+static uint16_t dmx_pan_sm_value = 0;
+static uint16_t dmx_tilt_sm_value = 0;
 
 static void dmx_channel_map(void)
 {
@@ -30,6 +37,46 @@ static void dmx_channel_map(void)
 	dmx_tilt_value = ((uint16_t)*(dmx_adress_pointer + (TILT_CH-1))<<8)  | ((uint16_t)*(dmx_adress_pointer + (TILT_F_CH-1)));
 	dmx_tilt_value =  (dmx_tilt_value >> 1);
 	dmx_speed_value = *(dmx_adress_pointer + (PT_SPEED_CH-1));
+}
+
+static void dmx_signal_smoothing(void)
+{
+	static uint16_t pan_next = 0;
+	static uint16_t pan_prev = 0;
+	static uint16_t tilt_next = 0;
+	static uint16_t tilt_prev = 0;
+
+
+	static uint32_t current_time = 0;
+	static uint32_t prev_time = 0;
+	static uint16_t count = 0;
+
+	current_time = HAL_GetTick();
+
+	if ((uint32_t)(current_time - prev_time) >= SM_TIME)	// időzítés
+	{
+		prev_time = current_time;
+		count++;
+
+		if(count >= SM_COUNT )								// mintavételezés
+		{
+			count = 0;
+
+			tilt_prev = tilt_next;
+			tilt_next = dmx_tilt_value;
+
+			pan_prev = pan_next;
+			pan_next = dmx_pan_value;
+		}
+
+		float step;											// interpolálás
+
+		step = ((float)tilt_next - tilt_prev) * SM_COUNT_REV; 	// lépéskülönbség / interp. szám
+		dmx_tilt_sm_value = (uint16_t)((float)tilt_prev + ((float)count * step));
+
+		step = ((float)pan_next - pan_prev) * SM_COUNT_REV; 	// lépéskülönbség / interp. szám
+		dmx_pan_sm_value = (uint16_t)((float)pan_prev + ((float)count * step));
+	}
 }
 
 void my_main_init(void)
@@ -43,12 +90,14 @@ void my_main_loop(void)
 
 	dmx_usart_send();
 
+	dmx_signal_smoothing();
+
 	if(reset_f)
 	{
 	 dmx_channel_map(); 			// normál futás
 
-	 motor_1_main(dmx_pan_value,  dmx_speed_value);
-	 motor_2_main(dmx_tilt_value, dmx_speed_value);
+	 motor_1_main(dmx_pan_sm_value,  dmx_speed_value);
+	 motor_2_main(dmx_tilt_sm_value, dmx_speed_value);
 	}
 	else							// reset
 	{
